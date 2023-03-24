436fce8006680f980357c838495e2df4c9019bfb
Taylor Y <tdy721@gmail.com>
2023-03-24 10:55:32 -0700

    link style

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,16 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { Options } from "$fresh/plugins/twind.ts"
+import { apply } from "twind"
 
 export default {
   selfURL: import.meta.url,
-} as Options;
+  preflight: {
+    a: apply`text-indigo text-underline visited:text-violet hover:text-blue`,
+  },
+  theme: {
+    colors: {
+      blue: "rgb(0,190,211)",
+      indigo: "rgb(0,153,212)",
+      violet: "rgb(108, 73, 136)",
+    },
+  },
+} as Options
