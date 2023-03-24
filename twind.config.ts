7e5a980dde753ad91b9c460bb7fac77af7786f18
Taylor Y <tdy721@gmail.com>
2023-03-24 10:58:24 -0700

    colorfix

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,6 @@
 import { Options } from "$fresh/plugins/twind.ts"
 import { apply } from "twind"
+import * as colors from "twind/colors"
 
 export default {
   selfURL: import.meta.url,
@@ -11,6 +12,10 @@ export default {
       blue: "rgb(0,190,211)",
       indigo: "rgb(0,153,212)",
       violet: "rgb(108, 73, 136)",
+      grey: "rgb(215, 215, 215)",
+      dark: "rgb(15,15,15)",
+      white: colors.white,
+      black: colors.black,
     },
   },
 } as Options
